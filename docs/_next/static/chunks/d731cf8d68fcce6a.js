5190a07f745fe370fb940a1035fcaf0678a817af
HIEU DOAN <49260251+hieudoanm@users.noreply.github.com>
2026-02-13 18:17:02 +0700

    add native

--- docs/_next/static/chunks/d731cf8d68fcce6a.js
+++ docs/_next/static/chunks/d731cf8d68fcce6a.js
@@ -2,7 +2,7 @@ __turbopack_load_page_chunks__("/_app", [
   "static/chunks/465a9fc9ece09e3a.js",
   "static/chunks/ae5e7607d6b86750.js",
   "static/chunks/7398cd937794733e.js",
-  "static/chunks/ca4e2dadb42a290b.css",
+  "static/chunks/0aff0e49f9aab2da.css",
   "static/chunks/be66d72b0409bf44.css",
-  "static/chunks/turbopack-80bc353c85bf71f8.js"
+  "static/chunks/turbopack-a5a522d101a8c159.js"
 ])
---
+++ packages/app/mobile/android/app/src/main/java/io/github/hieudoanm/qrcode/MainActivity.java
@@ -0,0 +1,5 @@
+package io.github.hieudoanm.qrcode;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
