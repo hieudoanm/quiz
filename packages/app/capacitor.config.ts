c14dbf69ba930d705963996f023c826edf3ff1aa
HIEU DOAN <49260251+hieudoanm@users.noreply.github.com>
2026-02-13 18:19:51 +0700

    update capacitor.config.ts

--- packages/app/capacitor.config.ts
+++ packages/app/capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.github.hieudoanm.qrcode',
-  appName: '@qrcode/frontend',
+  appId: 'io.github.hieudoanm.quiz',
+  appName: '@quiz/app',
   webDir: '../../docs',
   android: { path: 'mobile/android' },
   ios: { path: 'mobile/ios' },
