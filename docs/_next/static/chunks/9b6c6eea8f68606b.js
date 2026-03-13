4fb8073de57310b078b00e42cf9b0c47e252f66a
HIEU DOAN <49260251+hieudoanm@users.noreply.github.com>
2026-03-13 18:04:50 +0700

    add landing page

--- docs/_next/static/chunks/9b6c6eea8f68606b.js
+++ docs/_next/static/chunks/9b6c6eea8f68606b.js
@@ -2,7 +2,7 @@ __turbopack_load_page_chunks__("/_app", [
   "static/chunks/7c6843dcd4ff183e.js",
   "static/chunks/37a821232485c109.js",
   "static/chunks/72a5c9caf7c71589.js",
-  "static/chunks/84c5f38b94c023dc.css",
+  "static/chunks/a159b5c0a1c2fc2d.css",
   "static/chunks/be66d72b0409bf44.css",
-  "static/chunks/turbopack-39795e732d1676df.js"
+  "static/chunks/turbopack-0ca39094a9fd82a9.js"
 ])
